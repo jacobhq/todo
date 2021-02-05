3f2c3282d46ce4144273c29a4c6acf5f9b20367d
jacobhq <jacob.marshall.jem@gmail.com>
2021-02-05 22:13:40 +0000

    See code

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,12 @@ export default function IndexPage() {
       <div className="relative mx-auto">
           <p className="text-fgreen text-lg leading-none font-mono">✨ The most simple</p>
           <h1 className="text-fgreen huge leading-none mb-4">Todo list</h1>
-          <Link href="/app">
-              <a className="btn-green h-10 w-full absolute">Go</a>
-          </Link>
+          <div className="flex gap-2">
+              <Link href="/app">
+                <a className="btn-green-outline h-10 w-1/2">Go</a>
+              </Link>
+              <a className="btn-green h-10 w-1/2" href="https://github.com/jacobhq/todo" target="_blank">View source</a>
+          </div>
       </div>
     </div>
   )
